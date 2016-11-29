3f83669aa899dd51e8fd4f2139adeaddadfc3e37
Chad Clarke <chadclarke@bell.net>
2016-11-29 00:18:18 -0500

    Added Security Packages

---
+++ Startup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+//got to add some references 
+using Microsoft.AspNet.Identity;
+using Microsoft.Owin.Security.Cookies;
+
+[assembly: OwinStartup(typeof(workoutplanner.Startup))]
+
+namespace workoutplanner
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // set up the cookie config for tracking users 
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+                LoginPath = new PathString("/login.aspx")
+            });
+
+        }
+    }
+}
--- register.aspx.cs
+++ register.aspx.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+//identity reference 
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
 
 namespace workoutplanner
 {
@@ -16,7 +19,29 @@ namespace workoutplanner
 
         protected void btnRegister_Click(object sender, EventArgs e)
         {
+            // Use the connection from the web.config file to make a new identity user
+            var userStore = new UserStore<IdentityUser>();
+            var manager = new UserManager<IdentityUser>(userStore);
 
+            // populate the new user from the register form 
+            var user = new IdentityUser()
+            {
+                UserName = txtUsername.Text
+            };
+            //save the user 
+            IdentityResult result = manager.Create(user, txtPassword.Text);
+
+            // evaluate the result
+            if (result.Succeeded)
+            {
+
+                Response.Redirect("login.aspx");
+            }
+            else
+            {
+                lblMessage.Text = result.Errors.FirstOrDefault();
+                lblMessage.CssClass = "alert alert-danger";
+            }
         }
     }
 }
